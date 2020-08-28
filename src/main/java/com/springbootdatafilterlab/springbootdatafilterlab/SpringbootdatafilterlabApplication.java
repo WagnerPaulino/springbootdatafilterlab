602613596ece610f3a4b2c4d30a9214adec7b8c9
Wagner Paulino <wagnersantos9778@gmail.com>
2020-08-28 19:57:01 -0300

    initial commit

---
+++ src/main/java/com/springbootdatafilterlab/springbootdatafilterlab/SpringbootdatafilterlabApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdatafilterlab.springbootdatafilterlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdatafilterlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdatafilterlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdatafilterlab/springbootdatafilterlab/SpringbootdatafilterlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdatafilterlab.springbootdatafilterlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdatafilterlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
